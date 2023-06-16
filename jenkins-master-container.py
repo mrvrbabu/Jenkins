8a0f94d539fc8415ed279b2bce2ef18e392eae9f
mrvrbabu <mr.vrbabu@hotmail.com>
2023-06-16 07:57:52 +0530

    Modified jenkins file to exclude port 50000 and added .gitignore file

--- jenkins-master-container.py
+++ jenkins-master-container.py
@@ -1,24 +1,26 @@
-# Python script to install run jenkins as docker container 
-import os 
-import time 
+# Python script to install run jenkins as docker container
+import os
+import time
 
-# Export jenkins user and home directory 
+# Export jenkins user and home directory
 jenkins_user = "jenkins"
 home_directory = "/opt/jenkins"
 
-print("Jenkins user is",jenkins_user)
+print("Jenkins user is", jenkins_user)
 print("Jenkins home directory is", home_directory)
 
-# Add jenkins user 
+# Add jenkins user
 add_jenkins_user = "useradd -m -d /opt/jenkins jenkins"
 
-# Change jenkins home directory permissions 
+# Change jenkins home directory permissions
 home_dir_write_perm = "chmod -vR 777 /opt/jenkins"
 
 os.system(add_jenkins_user)
 os.system(home_dir_write_perm)
 
-docker_run = "docker run --name jenkins -it -d -p 8090:8080 -p 50000:50000 -v /opt/jenkins:/var/jenkins_home  jenkins/jenkins:lts-jdk11"
+
+# Note : - Enable port -p 50000:50000 to configure jenkins as master slave
+docker_run = "docker run --name jenkins -it -d --restart unless-stopped -p 8090:8080 -v /opt/jenkins:/var/jenkins_home  jenkins/jenkins:lts-jdk11"
 os.system(docker_run)
 print("\nPlease note, jenkins is accessible on port 8090\n")
 
